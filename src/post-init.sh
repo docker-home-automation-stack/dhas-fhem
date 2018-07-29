6546b47f4dca88e73c67d9c78947607debf1a767
Julian Pawlowski <julian@pawlowski.me>
2018-07-29 19:53:58 +0200

    TabletUI

--- src/post-init.sh
+++ src/post-init.sh
@@ -23,6 +23,7 @@ RET=$( cd ${FHEM_DIR}; perl fhem.pl 7072 "shutdown" 2>&1>/dev/null )
 
 # update fhem.cfg
 echo " - Pre-configuring fhem.cfg"
+cp -n ${FHEM_DIR}/www/tablet/index-example.html ${FHEM_DIR}/www/tablet/index.html
 cp -f ${FHEM_DIR}/fhem.cfg ${FHEM_DIR}/fhem.cfg.default-dhas
 perl /fhem-merge-config.pl "/src/fhem.cfg.tmpl/*.cfg" "${FHEM_DIR}/fhem.cfg"
 
