0bc09e1c41d3d046887bd2c144bf4b638e1b5d25
Julian Pawlowski <julian@pawlowski.me>
2018-07-30 14:13:03 +0200

    update fhem.cfg generation

--- src/fhem-merge-config.pl
+++ src/fhem-merge-config.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+use Data::Dumper;
+
 my $fhemCfgTmpl = $ARGV[0];
 my $fhemCfg     = $ARGV[1];
 my $CFG;
@@ -14,9 +16,11 @@ close($fh);
 @files = glob($fhemCfgTmpl);
 
 foreach my $file (@files) {
+
     open( my $fh, '<', $file ) or die "cannot open file $file";
     while ( my $line = <$fh> ) {
-        next if ( $line eq "\n" || $line =~ /^#/ );
+        next if ( $line eq "\n" || $line =~ /^[\s\t]*#/ );
+        chomp $line;
         my @words = split / /, $line;
         my $cmd   = shift @words;
         my $dev   = shift @words;
@@ -28,7 +32,7 @@ foreach my $file (@files) {
         if ( $cmd eq "define" ) {
             $mod = $typ;
         }
-        elsif ( $CFG =~ m/^define $dev (\w+|\W+).*\n/mi ) {
+        elsif ( $CFG =~ m/^define $dev ([A-Za-z]+).*\n/mi ) {
             $mod = $1;
         }
         elsif ( $dev ne "global" ) {
@@ -42,12 +46,17 @@ foreach my $file (@files) {
 
             # special handling for FHEMWEB devices
             if ( $mod eq "FHEMWEB" ) {
-                my $p = @{ split( " ", $val ) }[1];
+                my $p = ( split( " ", $val ) )[0];
 
                 # find existing definition with same port
-                if ( $CFG =~ m/^$cmd ([\w|\W]+) FHEMWEB $p ([\w|\W]+)?/ ) {
-                    $CFG =~ s/^attr $1.*\n//mi;
-                    $CFG =~ s/^define $1.*\n//mi;
+                if (    $CFG =~ m/^$cmd ([A-Za-z]+) FHEMWEB $p ([A-Za-z]+)?\n/mi
+                    and $dev ne $1 )
+                {
+                    my $d = $1;
+                    #print
+"    $dev: Removing dublicated FHEMWEB device $d for port $p\n";
+                    $CFG =~ s/^attr $d .*(\n+|$)//mi;
+                    $dev = $d;
                 }
 
             }
@@ -56,42 +65,52 @@ foreach my $file (@files) {
             if ( $CFG =~ m/^$cmd $dev $mod.*\n/mi ) {
 
                 #print "    $dev: Enforced device definition\n";
-                $CFG =~ s/^$cmd $dev $mod.*\n/$line/mi;
+                $CFG =~ s/^$cmd $dev $mod.*\n/$line\n/mi;
             }
 
             # add new define
             else {
                 #print "    $dev: Added new device\n";
-                $CFG .= "\n" . $line;
+                $CFG .= "\n" . $line . "\n";
             }
         }
 
         # handle attributes
-        if ( $cmd eq "attr" ) {
+        elsif ( $cmd eq "attr" ) {
 
             # overwrite existing attribute
             if ( $CFG =~ m/^$cmd $dev $typ .+\n/mi ) {
 
                 #print "    $dev: Enforced attribute '$typ'\n";
-                $CFG =~ s/^$cmd $dev $typ .+\n/$line/mi;
+                $CFG =~ s/^$cmd $dev $typ .+\n/$line\n/mi;
             }
             elsif ( $CFG =~ m/^define $dev $mod.*\n/mi ) {
 
                 #print "    $dev: New attribute '$typ'\n";
-                $CFG =~ s/^(define $dev $mod.*\n)/$1$line/mi;
+                $CFG =~ s/^(define $dev $mod.*\n)/$1$line\n/mi;
             }
             elsif ( $dev eq "global" ) {
 
                 #print "    $dev: New GLOBAL attribute '$typ'\n";
-                $CFG =~ s/^(attr $dev .*\n)/$1$line/mi;
+                $CFG =~ s/^(attr $dev .*\n)/$1$line\n/mi;
             }
 
         }
 
         # delete
-        if ( $cmd eq "-delete" ) {
-            $CFG =~ s/^attr $dev.*\n//mi;
-            $CFG =~ s/^define $dev.*\n//mi;
+        elsif ( $cmd eq "-delete" ) {
+            $CFG =~ s/^attr $dev .*(\n+|$)//mi;
+            $CFG =~ s/^define $dev .*(\n+|$)//mi;
+        }
+
+        # delete
+        elsif ( $cmd eq "-define" ) {
+            $CFG =~ s/^define $dev .*(\n+|$)//mi;
+        }
+
+        # delete
+        elsif ( $cmd eq "-attr" ) {
+            $CFG =~ s/^attr $dev .*(\n+|$)//mi;
         }
 
     }
--- src/post-init.sh
+++ src/post-init.sh
@@ -9,7 +9,7 @@ echo " - Adding sources to controls.txt"
 cp /src/controls.txt ${FHEM_DIR}/FHEM/controls.txt
 
 # Enable sources
-echo " - Installing add-ons"
+echo " - Running initial update, this may take a while"
 RET=$( cd ${FHEM_DIR}; perl fhem.pl fhem.cfg )
 while ! nc -z localhost 7072; do
   sleep 0.5
@@ -23,6 +23,7 @@ RET=$( cd ${FHEM_DIR}; perl fhem.pl 7072 "shutdown" 2>&1>/dev/null )
 
 # update fhem.cfg
 echo " - Pre-configuring fhem.cfg"
+cp -n /src/db.conf ${FHEM_DIR}/db.conf
 cp -n ${FHEM_DIR}/www/tablet/index-example.html ${FHEM_DIR}/www/tablet/index.html
 cp -f ${FHEM_DIR}/fhem.cfg ${FHEM_DIR}/fhem.cfg.default-dhas
 perl /fhem-merge-config.pl "/src/fhem.cfg.tmpl/*.cfg" "${FHEM_DIR}/fhem.cfg"
